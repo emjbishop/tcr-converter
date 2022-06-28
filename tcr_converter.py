909cdda88f0565e0fc910af41ee2a257b46d0856
Emma Bishop <bishemma1@gmail.com>
2022-06-28 15:54:35 -0700

    Detect input format using headers
    Looks for interesection(s) between set of input headers and sets of
    known headers for adaptive v4 and airr.

--- tcr_converter.py
+++ tcr_converter.py
@@ -42,16 +42,149 @@ def main():
     extra_columns = args.extra
     organism = args.species
 
-    df = load_data(input)
+    format = get_input_format(in_file=input)
 
-    if type == '10x':
-        out_df = tenx_to_imgt(df, chains)
-    elif type == 'adaptive':
-        out_df = adaptive_to_imgt(df, extra_columns, chains, organism)
-    elif type == 'airr':
+    if format == 'adaptive_v4':
+        df = load_data(input)
+        out_df = adapt_to_imgt(df, extra_columns, chains, organism)
+    elif format == 'airr':
+        df = load_data(input)
         out_df = airr_to_imgt(df, extra_columns, chains)
+    else:
+        print('Only AIRR and Adaptive v4 supported at this time.')
+        sys.exit(0)
     
-    out_df.to_csv(outpath, sep='\t', index=False)
+    print(f'Writing output to: "{outpath}"')
+    out_df.to_csv(outpath, index=False)
+
+      
+# Columns unique to these formats
+headers = {
+    # https://clients.adaptivebiotech.com/assets/downloads/
+    # immunoSEQ_AnalyzerManual.pdf 
+    'adaptive_v2': {'nucleotide', 'aminoAcid', 'count (templates/reads)',
+                'frequencyCount (%)', 'cdr3Length', 'vMaxResolved', 
+                'vFamilyName', 'vGeneName', 'vGeneAllele', 'vFamilyTies', 
+                'vGeneNameTies', 'vGeneAlleleTies', 'dMaxResolved', 
+                'dFamilyName', 'dGeneName', 'dGeneAllele', 'dFamilyTies', 
+                'dGeneNameTies', 'dGeneAlleleTies', 'jMaxResolved', 
+                'jFamilyName', 'jGeneName', 'jGeneAllele', 'jFamilyTies', 
+                'jGeneNameTies', 'jGeneAlleleTies', 'vDeletion', 'n1Insertion', 
+                'd5Deletion', 'd3Deletion', 'n2Insertion', 'jDeletion',
+                'vIndex', 'n1Index', 'dIndex', 'n2Index', 'jIndex',
+                'estimatedNumberGenomes', 'sequenceStatus', 'cloneResolved', 
+                'vOrphon', 'dOrphon', 'jOrphon', 'vFunction', 'dFunction', 
+                'jFunction', 'fractionNucleated', 'vAlignLength', 
+                'vAlignSubstitutionCount', 'vAlignSubstitutionIndexes', 
+                'vAlignSubstitutionGeneThreePrimeIndexes', 'vSeqWithMutations'},
+
+    # https://www.adaptivebiotech.com/wp-content/uploads/2019/07/
+    # MRK-00342_immunoSEQ_TechNote_DataExport_WEB_REV.pdf
+    'adaptive_v4': {'rearrangement', 'extended_rearrangement', 'bio_identity', 
+                'amino_acid','templates', 'frame_type', 'rearrangement_type', 
+                'productive_frequency', 'cdr1_start_index', 
+                'cdr1_rearrangement_length', 'cdr2_start_index', 
+                'cdr2_rearrangement_length', 'cdr3_start_index', 'cdr3_length',
+                'v_index', 'n1_index', 'd_index', 'n2_index', 'j_index', 
+                'v_deletions', 'n2_insertions', 'd3_deletions', 'd5_deletions', 
+                'n1_insertions', 'j_deletions', 'chosen_j_allele', 
+                'chosen_j_family', 'chosen_j_gene', 'chosen_v_allele', 
+                'chosen_v_family', 'chosen_v_gene', 'd_allele', 
+                'd_allele_ties', 'd_family', 'd_family_ties', 'd_gene_ties',
+                'd_resolved', 'j_allele', 'j_allele_ties', 'j_family', 
+                'j_family_ties', 'j_gene_ties', 'j_resolved', 'v_allele', 
+                'v_allele_ties', 'v_family', 'v_family_ties', 'v_gene_ties', 
+                'v_resolved', 'Frequency', 'Reads', 'Rearrangement', 
+                'v_shm_count', 'v_shm_indexes', 'Antibody'},
+
+    # https://support.10xgenomics.com/single-cell-vdj/software/pipelines/
+    # latest/output/annotation#contig-annotation
+    '10x': {'barcode', 'is_cell', 'contig_id', 'high_confidence', 'length', 
+            'chain', 'c_gene', 'full_length', 'fwr1_nt', 'cdr1_nt', 'fwr2_nt', 
+            'cdr2_nt', 'fwr3_nt', 'cdr3_nt', 'fwr4_nt', 'reads', 'umis', 
+            'raw_clonotype_id', 'raw_consensus_id', 'exact_subclonotype_id'},
+    
+    # From a real file
+    'airr': {'sequence_id', 'sequence', 'sequence_aa', 'rev_comp', 'vj_in_frame', 
+            'stop_codon', 'complete_vdj', 'locus', 'v_call', 'd_call', 'd2_call', 
+            'j_call', 'c_call', 'sequence_alignment', 'sequence_alignment_aa', 
+            'germline_alignment', 'germline_alignment_aa', 'junction', 'junction_aa', 
+            'np1', 'np1_aa', 'np2', 'np2_aa', 'np3', 'np3_aa', 'cdr1_aa', 'cdr2_aa', 
+            'cdr3_aa', 'fwr1_aa', 'fwr2_aa', 'fwr3_aa', 'fwr4_aa', 'v_score', 
+            'v_identity', 'v_support', 'v_cigar', 'd_score', 'd_identity',
+            'd_support', 'd_cigar', 'd2_score', 'd2_identity', 'd2_support',
+            'd2_cigar', 'j_score', 'j_identity', 'j_support', 'j_cigar', 'c_score',
+            'c_identity', 'c_support', 'c_cigar', 'v_sequence_start',
+            'v_sequence_end', 'v_germline_start', 'v_germline_end',
+            'v_alignment_start', 'v_alignment_end', 'd_sequence_start',
+            'd_sequence_end', 'd_germline_start', 'd_germline_end',
+            'd_alignment_start', 'd_alignment_end', 'd2_sequence_start',
+            'd2_sequence_end', 'd2_germline_start', 'd2_germline_end',
+            'd2_alignment_start', 'd2_alignment_end', 'j_sequence_start',
+            'j_sequence_end', 'j_germline_start', 'j_germline_end',
+            'j_alignment_start', 'j_alignment_end', 'cdr1_start', 'cdr1_end',
+            'cdr2_start', 'cdr2_end', 'cdr3_start', 'cdr3_end', 'fwr1_start',
+            'fwr1_end', 'fwr2_start', 'fwr2_end', 'fwr3_start', 'fwr3_end',
+            'fwr4_start', 'fwr4_end', 'v_sequence_alignment',
+            'v_sequence_alignment_aa', 'd_sequence_alignment',
+            'd_sequence_alignment_aa', 'd2_sequence_alignment',
+            'd2_sequence_alignment_aa', 'j_sequence_alignment',
+            'j_sequence_alignment_aa', 'c_sequence_alignment',
+            'c_sequence_alignment_aa', 'v_germline_alignment',
+            'v_germline_alignment_aa', 'd_germline_alignment',
+            'd_germline_alignment_aa', 'd2_germline_alignment',
+            'd2_germline_alignment_aa', 'j_germline_alignment',
+            'j_germline_alignment_aa', 'c_germline_alignment',
+            'c_germline_alignment_aa', 'junction_length', 'junction_aa_length',
+            'np1_length', 'np2_length', 'np3_length', 'n1_length', 'n2_length',
+            'n3_length', 'p3v_length', 'p5d_length', 'p3d_length', 'p5d2_length',
+            'p3d2_length', 'p5j_length', 'consensus_count', 'duplicate_count',
+            'cell_id', 'clone_id', 'rearrangement_id', 'repertoire_id',
+            'sample_processing_id', 'data_processing_id', 'germline_database',
+            'rearrangement_set_id'}
+}
+
+
+def load_header(in_file):
+    '''Returns set of column headers'''
+    load = {'.xlsx': pd.read_excel,
+            '.csv': pd.read_csv,
+            '.tsv': pd.read_table}
+    ext = str(os.path.splitext(in_file)[1])
+    in_cols = load[ext](in_file, index_col=0, nrows=0).columns
+    in_cols = set(in_cols)
+    return in_cols
+
+
+def get_input_format(in_file):
+    '''Detects standard format based on column headers'''
+    in_headers = load_header(in_file=in_file)
+    format = []
+
+    # Check for intersections (that aren't empty sets) with known headers
+    if not in_headers.isdisjoint(headers['10x']):
+        format.append('10x')
+    if not in_headers.isdisjoint(headers['adaptive_v2']):
+        format.append('adaptive_v2')
+    if not in_headers.isdisjoint(headers['adaptive_v4']):
+        format.append('adaptive_v4')
+    if not in_headers.isdisjoint(headers['airr']):
+        format.append('airr')
+
+    # If doesn't intersect with any
+    if len(format) == 0:
+        print('Input file does not have any standard headers, please convert by '\
+            'hand. Exiting now.')
+        sys.exit(0)
+    # If intersects with multiple
+    elif len(format) > 1:
+        print('Found column names from multiple formats:', format, '\nInput file'\
+            'is not a standard format, please convert by hand. Exiting now.')
+        sys.exit(0)
+    else:
+        print('Detected format:', format[0])
+
+    return format[0]
 
 
 def load_data(in_file):
